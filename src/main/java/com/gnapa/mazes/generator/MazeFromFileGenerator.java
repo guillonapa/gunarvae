d7a18ccb900d7252db7bec1fb2c5e9fa6aa4bd62
Guillermo Narvaez-Paliza <guillonapa@gmail.com>
2020-10-20 07:36:52 -0400

    :sparkles: Allow maze files to include entrances and exits

--- src/main/java/com/gnapa/mazes/generator/MazeFromFileGenerator.java
+++ src/main/java/com/gnapa/mazes/generator/MazeFromFileGenerator.java
@@ -13,15 +13,16 @@ import com.gnapa.mazes.utils.Pair;
 /**
  * <p>
  * Class to generate mazes or rooms from file representations of them.
- * Every character '#' in the file is considered a wall, while every
- * other character is considered a space. The number of columns will be
- * equal to the maximum line length seen while parsing the file, while
+ * Every character '#' in the file is considered a wall, a '?' character
+ * is considered an entrance, and a '!' character is considered an exit,
+ * while every other character is considered a space. The number of columns 
+ * will be equal to the maximum line length seen while parsing the file, while
  * the number of rows will be equal to the number of rows in the file.
  * Any missing characters will be interpreted as space. An example:
  * </p>
  * 
  * <pre>
- * ###### #########
+ * ######?#########
  * ##            ##
  * ###### ###### ##
  * ###### #########
@@ -41,7 +42,7 @@ import com.gnapa.mazes.utils.Pair;
  * ## ##   ### ####
  * ## ### ####    #
  * ##     ####### #
- * ############## #
+ * ##############!#
  * </pre>
  * 
  * @author Guillermo Narvaez
@@ -68,6 +69,12 @@ public class MazeFromFileGenerator {
         // the walls in the maze
         List<Pair> walls = new LinkedList<>();
         
+        // the entrances in the maze
+        List<Pair> entrances = new LinkedList<>();
+        
+        // the entrances in the maze
+        List<Pair> exits = new LinkedList<>();
+        
         // read each character from each line
         int row = 0;
         int maxColumns = 0;
@@ -77,6 +84,8 @@ public class MazeFromFileGenerator {
             // traverse the line
             for (int column = 0; column < line.length(); column++) {
                 if (line.charAt(column) == '#') walls.add(Pair.create(row, column));
+                if (line.charAt(column) == '?') entrances.add(Pair.create(row, column));
+                if (line.charAt(column) == '!') exits.add(Pair.create(row, column));
             }
             row++;
         }
@@ -87,6 +96,16 @@ public class MazeFromFileGenerator {
         // generate the maze
         MazeGenerator generator = new MazeGenerator();
         String[][] maze = generator.generate(maxRows, maxColumns, false);
+        
+        // apply all entrances
+        for (Pair coors : entrances) {
+            maze = generator.entrance(maze, coors.getRow(), coors.getColumn());
+        }
+        
+        // apply all exits
+        for (Pair coors : exits) {
+            maze = generator.exit(maze, coors.getRow(), coors.getColumn());
+        }
         
         // apply the walls to the new maze
         return generator.applyWalls(maze, walls);
